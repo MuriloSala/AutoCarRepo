f86a8b1d192b05551226f029ff18fac75ab8632c
Murilo Sala <murilo.sala022@gmail.com>
2025-09-05 10:27:10 -0300

    Adicionados getters e setters das classes filhas.

--- src/ClasseCarros.java
+++ src/ClasseCarros.java
@@ -6,5 +6,39 @@ public class ClasseCarros extends ClasseAutomotores {
 
     public ClasseCarros(String corAt, String marcaAt, String modeloAt, String tipoAt) {
         super(corAt, marcaAt, modeloAt, tipoAt);
+ 
     }
+    
+    public String getCorAt() {
+        return corAt;
+    }
+
+    public void setCorAt(String corAt) {
+        this.corAt = corAt;
+    }
+
+    public String getMarcaAt() {
+        return marcaAt;
+    }
+
+    public void setMarcaAt(String marcaAt) {
+        this.marcaAt = marcaAt;
+    }
+
+    public String getModeloAt() {
+        return modeloAt;
+    }
+
+    public void setModeloAt(String modeloAt) {
+        this.modeloAt = modeloAt;
+    }
+
+    public String getTipoAt() {
+        return tipoAt;
+    }
+
+    public void setTipoAt(String tipoAt) {
+        this.tipoAt = tipoAt;
+    }
+
 }
